b89323d5cc53348aaa36417a44a8c4e86fc86a98
skynowa <skynowa@gmail.com>
2024-03-11 17:35:08 +0200

    single-app: main.cpp (+4) [115+/3-] - impl

--- qLang/Config.h
+++ qLang/Config.h
@@ -11,6 +11,20 @@
 #include <QTimer>
 #include <QWidget>
 #include <QCursor>
+
+#include <iostream>
+#include <unistd.h>
+
+#include <X11/Xlib.h>
+#include <X11/XKBlib.h>
+#include <X11/cursorfont.h>
+#include <X11/Xutil.h>
+#include <X11/Xos.h>
+#include <X11/Xatom.h>
+#include <X11/Xutil.h>
+
+#include <StdStream/StdStream.h>
+#include <StdTest/StdTest.h>
 //-------------------------------------------------------------------------------------------------
 #define ICONS_DIR "/home/skynowa/Projects/X11KeboardLayouts/qLang/Data"
 //-------------------------------------------------------------------------------------------------
