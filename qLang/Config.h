2d5cae977822b9a4495eb67060d9ccaefcad1950
skynowa <skynowa@gmail.com>
2024-03-13 00:00:09 +0200

    single-app: Config.h [1+/8-] - review

--- qLang/Config.h
+++ qLang/Config.h
@@ -8,18 +8,11 @@
 
 //-------------------------------------------------------------------------------------------------
 #include <QApplication>
-#include <QTimer>
 #include <QWidget>
+#include <QTimer>
 #include <QCursor>
 
-#include <unistd.h>
-
-#include <X11/Xlib.h>
 #include <X11/XKBlib.h>
-#include <X11/cursorfont.h>
-#include <X11/Xutil.h>
-#include <X11/Xos.h>
-#include <X11/Xatom.h>
 #include <X11/Xutil.h>
 
 #include <StdStream/StdStream.h>
